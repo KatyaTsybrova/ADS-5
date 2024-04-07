0bfe1930fc10e0a47c6b541d4e3d1923db2c4dd1
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-04-07 18:52:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,75 @@
 #include <map>
 #include "tstack.h"
 
+using namespace std;
+
+int precedence(char op) {
+    if (op == '+' || op == '-') {
+        return 1;
+    } else if (op == '*' || op == '/') {
+        return 2;
+    }
+    return 0;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
+  string pst = "";
+    stack<char> s;
+
+    for (char c : inf) {
+        if (isalnum(c)) {
+            pst += c;
+        } else if (c == '(') {
+            s.push(c);
+        } else if (c == ')') {
+            while (!s.empty() && s.top() != '(') {
+                pst += s.top();
+                s.pop();
+            }
+            s.pop();
+        } else {
+            while (!s.empty() && precedence(s.top()) >= precedence(c)) {
+                pst += s.top();
+                s.pop();
+            }
+            s.push(c);
+        }
+    }
+
+    while (!s.empty()) {
+        pst += s.top();
+        s.pop();
+    }
   return std::string("");
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  stack<int> s;
+
+    for (char c : post) {
+        if (isdigit(c)) {
+            s.push(c - '0');
+        } else {
+            int operand2 = s.top();
+            s.pop();
+            int operand1 = s.top();
+            s.pop();
+
+            switch (c) {
+                case '+':
+                    s.push(operand1 + operand2);
+                    break;
+                case '-':
+                    s.push(operand1 - operand2);
+                    break;
+                case '*':
+                    s.push(operand1 * operand2);
+                    break;
+                case '/':
+                    s.push(operand1 / operand2);
+                    break;
+            }
+        }
+    }
+    return s.top();
 }
